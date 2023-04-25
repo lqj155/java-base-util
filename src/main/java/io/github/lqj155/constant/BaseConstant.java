d62cfb1effa7662cf767fa37ee14bc6ccb4110f2
liuquanjun <1558325811@qq.com>
2023-04-25 09:43:46 +0800

    123

--- src/main/java/io/github/lqj155/constant/BaseConstant.java
+++ src/main/java/io/github/lqj155/constant/BaseConstant.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseConstant {
 
+
     public static final String EMPTY = "";
 
     public static final String comma = ",";
